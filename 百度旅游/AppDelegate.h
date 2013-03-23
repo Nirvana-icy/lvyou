59ce94a8bbbd525d6b528a2cc232c54db7c8f1a4
Lucky <LJS@LJSMacBook-Pro.local>
2013-03-23 13:50:25 +0800

    Initial Commit

---
+++ 百度旅游/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  百度旅游
+//
+//  Created by Lucky on 13-3-23.
+//  Copyright (c) 2013年 Hurricane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 百度旅游/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  百度旅游
+//
+//  Created by Lucky on 13-3-23.
+//  Copyright (c) 2013年 Hurricane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
